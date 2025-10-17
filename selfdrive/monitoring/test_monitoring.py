af94ca42ff48ad6e8be105c78c0170c9fae109ea
firestar5683 <168790843+firestar5683@users.noreply.github.com>
2025-10-16 23:49:36 -0500

    Hurts Donut

--- selfdrive/monitoring/test_monitoring.py
+++ selfdrive/monitoring/test_monitoring.py
@@ -24,8 +24,6 @@ def make_msg(face_detected, distracted=False, model_uncertain=False):
   ds.leftDriverData.rightBlinkProb = 1. * distracted
   ds.leftDriverData.faceOrientationStd = [1.*model_uncertain, 1.*model_uncertain, 1.*model_uncertain]
   ds.leftDriverData.facePositionStd = [1.*model_uncertain, 1.*model_uncertain]
-  # TODO: test both separately when e2e is used
-  ds.leftDriverData.readyProb = [0., 0., 0., 0.]
   ds.leftDriverData.notReadyProb = [0., 0.]
   return ds
 
