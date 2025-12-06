3664aa3e91ec72e23727ea326d654517080b2ac6
firestar5683 <168790843+firestar5683@users.noreply.github.com>
2025-12-06 16:37:52 -0600

    Change NoEntryAlert message to 'Reverse Gear'

--- selfdrive/controls/lib/events.py
+++ selfdrive/controls/lib/events.py
@@ -983,7 +983,7 @@ EVENTS: dict[int, dict[str, Alert | AlertCallbackType]] = {
       AlertStatus.normal, AlertSize.full,
       Priority.LOWEST, VisualAlert.none, AudibleAlert.none, .2, creation_delay=0.5),
     ET.USER_DISABLE: ImmediateDisableAlert("Wrong Gear"),
-    ET.NO_ENTRY: NoEntryAlert("Wrong Gear"),
+    ET.NO_ENTRY: NoEntryAlert("Reverse Gear"),
   },
 
   # On cars that use stock ACC the car can decide to cancel ACC for various reasons.
